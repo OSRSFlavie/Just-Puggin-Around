7b2f5c22cdd96e2ac561f7d636d2fc0cf2e1053e
t <t@t.t>
2026-09-16 09:32:58 -0400

    1.0.2a

--- src/test/java/com/osrsflavie/justpugginaround/JustPugginAroundPluginTest.java
+++ src/test/java/com/osrsflavie/justpugginaround/JustPugginAroundPluginTest.java
@@ -5,6 +5,7 @@ import net.runelite.client.externalplugins.ExternalPluginManager;
 
 public class JustPugginAroundPluginTest
 {
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) throws Exception
     {
         /*
