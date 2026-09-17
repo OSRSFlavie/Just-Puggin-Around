c3594b3b74ad58238cec4f53d32d665a1029715b
t <t@t.t>
2026-09-16 21:07:26 -0400

    Modified the audio player to comply with Runelite's submission feedback.

--- src/main/java/com/osrsflavie/justpugginaround/JustPugginAroundPlugin.java
+++ src/main/java/com/osrsflavie/justpugginaround/JustPugginAroundPlugin.java
@@ -2,15 +2,10 @@ package com.osrsflavie.justpugginaround;
 
 import com.google.inject.Provides;
 
-import java.io.BufferedInputStream;
-import java.io.InputStream;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadLocalRandom;
 
 import javax.inject.Inject;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.FloatControl;
 
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
@@ -18,6 +13,7 @@ import net.runelite.api.NPC;
 import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.GameTick;
+import net.runelite.client.audio.AudioPlayer;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
@@ -43,8 +39,11 @@ public class JustPugginAroundPlugin extends Plugin
     @Inject
     private JustPugginAroundConfig config;
 
-    private Clip pugTiredClip;
-    private Clip puppyTiredClip;
+    @Inject
+    private AudioPlayer audioPlayer;
+
+    @Inject
+    private ScheduledExecutorService executorService;
 
     private WorldPoint previousLocation;
     private int tilesTravelled;
@@ -56,21 +55,12 @@ public class JustPugginAroundPlugin extends Plugin
     {
         resetTracking();
 
-        pugTiredClip = loadClip(PUG_TIRED_SOUND);
-        puppyTiredClip = loadClip(PUPPY_TIRED_SOUND);
-
         log.info("Just Puggin' Around started");
     }
 
     @Override
     protected void shutDown()
     {
-        closeClip(pugTiredClip);
-        pugTiredClip = null;
-
-        closeClip(puppyTiredClip);
-        puppyTiredClip = null;
-
         resetTracking();
 
         log.info("Just Puggin' Around stopped");
@@ -240,127 +230,39 @@ public class JustPugginAroundPlugin extends Plugin
         if (ThreadLocalRandom.current().nextInt(100) == 0)
         {
             log.info("Rare tired sound triggered: {}", PUPPY_TIRED_SOUND);
-            playTiredSound(puppyTiredClip, PUPPY_TIRED_SOUND);
+            playTiredSound(PUPPY_TIRED_SOUND);
         }
         else
         {
-            playTiredSound(pugTiredClip, PUG_TIRED_SOUND);
+            playTiredSound(PUG_TIRED_SOUND);
         }
     }
 
     /**
-     * Plays an already-loaded audio clip.
-     * No audio decoding or Clip creation occurs on the GameTick path.
+     * Plays a bundled audio resource using RuneLite's audio manager.
      */
-    private void playTiredSound(Clip clip, String soundResource)
+    private void playTiredSound(String soundResource)
     {
-        if (clip == null)
-        {
-            log.warn(
-                    "Unable to play {} because the audio clip was not loaded",
-                    soundResource
-            );
-            return;
-        }
-
-        if (clip.isRunning())
-        {
-            return;
-        }
-
-        try
-        {
-            applyVolume(clip);
-            clip.setFramePosition(0);
-            clip.start();
-        }
-        catch (Exception ex)
-        {
-            log.warn("Unable to play {}", soundResource, ex);
-        }
-    }
-
-    /**
-     * Loads a classpath audio resource into a Clip during plugin startup.
-     */
-    private Clip loadClip(String soundResource)
-    {
-        try (InputStream inputStream = getClass().getResourceAsStream(soundResource))
-        {
-            if (inputStream == null)
-            {
-                log.warn("Sound resource was not found: {}", soundResource);
-                return null;
-            }
-
-            try (BufferedInputStream bufferedInputStream =
-                         new BufferedInputStream(inputStream);
-                 AudioInputStream audioInputStream =
-                         AudioSystem.getAudioInputStream(bufferedInputStream))
-            {
-                Clip clip = AudioSystem.getClip();
-                clip.open(audioInputStream);
-
-                return clip;
-            }
-        }
-        catch (Exception ex)
-        {
-            log.warn("Unable to load sound resource {}", soundResource, ex);
-            return null;
-        }
-    }
-
-    /**
-     * Applies the configured volume to an already-loaded clip.
-     */
-    private void applyVolume(Clip clip)
-    {
-        if (!clip.isControlSupported(FloatControl.Type.MASTER_GAIN))
-        {
-            return;
-        }
-
-        FloatControl gainControl =
-                (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-
         int volume = config.volume();
 
         if (volume <= 0)
         {
-            gainControl.setValue(gainControl.getMinimum());
             return;
         }
 
         float gain = (float) (20.0 * Math.log10(volume / 100.0));
 
-        gain = Math.max(
-                gainControl.getMinimum(),
-                Math.min(gainControl.getMaximum(), gain)
-        );
-
-        gainControl.setValue(gain);
-    }
-
-    /**
-     * Stops and closes a loaded audio clip.
-     */
-    private void closeClip(Clip clip)
-    {
-        if (clip == null)
+        executorService.execute(() ->
         {
-            return;
-        }
-
-        try
-        {
-            clip.stop();
-            clip.close();
-        }
-        catch (Exception ex)
-        {
-            log.debug("Error while closing audio clip", ex);
-        }
+            try
+            {
+                audioPlayer.play(JustPugginAroundPlugin.class, soundResource, gain);
+            }
+            catch (Exception ex)
+            {
+                log.warn("Unable to play {}", soundResource, ex);
+            }
+        });
     }
 
     private void resetTracking()
